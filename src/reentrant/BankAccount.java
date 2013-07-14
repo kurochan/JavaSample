a3ba0c004045f59773f392342e9dc5ebf63ae3b3
kurochan <kurochan@example.com>
2013-07-14 16:56:24 +0900

    first commit

---
+++ src/reentrant/BankAccount.java
@@ -0,0 +1,42 @@
+package reentrant;
+
+interface BankAccount {
+	public int getBalances();
+	public int depositMoney(String name, int money);
+}
+
+class BadBankAccount implements BankAccount {
+	private int money;
+
+	public int getBalances() {
+		return money;
+	}
+
+	public int depositMoney(String name, int money) {
+		int tmp = this.money;
+		System.out.println(name + ": 入金前の残高は " + tmp + "円です");
+		System.out.println(name + ": 入金作業中です");
+		tmp += money;
+		this.money = tmp;
+		System.out.println(name + ": 入金後の残高は " + tmp + "円です");
+		return tmp;
+	}
+}
+
+class GoodBankAccount implements BankAccount {
+	private int money;
+
+	public int getBalances() {
+		return money;
+	}
+
+	public synchronized int depositMoney(String name, int money) {
+		int tmp = this.money;
+		System.out.println(name + ": 入金前の残高は " + tmp + "円です");
+		System.out.println(name + ": 入金作業中です");
+		tmp += money;
+		this.money = tmp;
+		System.out.println(name + ": 入金後の残高は " + tmp + "円です");
+		return tmp;
+	}
+}
---
+++ src/reentrant/BankSample.java
@@ -0,0 +1,42 @@
+package reentrant;
+
+public class BankSample extends Thread {
+	private String name;
+	private BankAccount bank;
+
+	BankSample(String name, BankAccount bank) {
+		this.name = name;
+		this.bank = bank;
+	}
+
+	public void run() {
+		for (int i = 0; i < 100; i++) {
+			System.out.println(name + ": " + (i + 1) + " 回目");
+			bank.depositMoney(name, 100);
+		}
+	}
+
+	public static void main(String[] args) {
+		 BankAccount bank = new BadBankAccount();
+		//BankAccount bank = new GoodBankAccount();
+		System.out.println("口座を初期化しました");
+		System.out.println("残高は " + bank.getBalances() + "円です");
+
+		BankSample a = new BankSample("A", bank);
+		BankSample b = new BankSample("B", bank);
+		BankSample c = new BankSample("C", bank);
+
+		a.start();
+		b.start();
+		c.start();
+		try {
+			a.join();
+			b.join();
+			c.join();
+		} catch (InterruptedException e) {
+		}
+
+		System.out.println("処理が終わりました");
+		System.out.println("残高は " + bank.getBalances() + "円です");
+	}
+}
