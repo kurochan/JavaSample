8effd73fd721ea0c861498f0c9cd76a7beb70cf3
kurochan <kurochan@example.com>
2013-07-19 17:44:54 +0900

    HelloWorldサンプルを追加

---
+++ src/hello/HelloJava.java
@@ -0,0 +1,13 @@
+// パッケージ宣言
+package hello;
+
+// クラス宣言
+public class HelloJava {
+
+	// Javaでは、実行時、最初にpublic static void main(String[] args)というメソッドが呼ばれる
+	public static void main(String[] args) {
+		// Hello World!と出力する
+		System.out.println("Hello World!");
+	}
+
+}
